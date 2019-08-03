2f650e95a36e43ce50a0fc0808b7f415aad4fc1e
Michał Bąkowski <michal.bakowski@northslope.pl>
2019-08-03 15:10:00 +0200

    Added `dotenv` package. Added `.env` file. Using env variables PORT + HOSTNAME

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
+const PORT = process.env.PORT || 3000;
+const HOSTNAME = process.env.HOSTNAME || "localhost";
+
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    await app.listen(3000);
+    await app.listen(PORT, HOSTNAME);
 }
 bootstrap();
