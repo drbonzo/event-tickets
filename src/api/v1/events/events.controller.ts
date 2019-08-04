2e622235dadc727738437b4407c631c825286f8a
Michał Bąkowski <michal.bakowski@northslope.pl>
2019-08-04 13:52:05 +0200

    Added `GET /api/v1/events/{id}` - details of single Event, with TicketTypes and Ticket counts grouped by Ticket.state

--- src/api/v1/events/events.controller.ts
+++ src/api/v1/events/events.controller.ts
@@ -1,14 +1,33 @@
-import { Controller, Get, Inject } from "@nestjs/common";
+import { Controller, Get, Inject, NotFoundException, Param } from "@nestjs/common";
 import { DATABASE_CONNECTION } from "../../../providers/provider-names";
 import { Connection } from "typeorm";
 import { EventEntity } from "../../../entity/EventEntity";
-import { TICKET_STATUS_AVAILABLE } from "../../../entity/Ticket";
+import {
+    TICKET_STATUS_AVAILABLE,
+    TICKET_STATUS_RESERVED,
+    TICKET_STATUS_SOLD,
+} from "../../../entity/Ticket";
 
 interface EventWithTicketCounts {
     id: number;
     name: string;
     startDateTime: number; // unix timestamp
     availableTicketsCount: number;
+}
+
+interface TicketTypeWithTicketCounts {
+    id: number;
+    name: string;
+    price: number;
+    sellingOption: string;
+    availableTicketCount: number;
+    reservedTicketCount: number;
+    soldTicketCount: number;
+}
+
+interface EventWithDetails {
+    event: EventEntity;
+    ticketTypes: TicketTypeWithTicketCounts[];
 }
 
 @Controller("/api/v1/events")
@@ -21,7 +40,7 @@ export class EventsController {
 
         const now = Date.now();
 
-        // TODO Uses corelated subquery - may need optimization
+        // TODO Uses corelated subquery - fetch all Events and then do separate count of Tickets + merge results
         const eventWithTicketCounts: EventWithTicketCounts[] = await eventRepository.query(
             `SELECT event_entity.id,
                     event_entity.name,
@@ -41,5 +60,34 @@ export class EventsController {
 
         // TODO format date time: event.startDateTime
         return eventWithTicketCounts;
+    }
+
+    @Get("/:id")
+    public async getEventDetails(@Param("id") id: number): Promise<EventWithDetails> {
+        const eventRepository = this.databaseConnection.getRepository(EventEntity);
+
+        const event: EventEntity | undefined = await eventRepository.findOne(id);
+
+        if (event === undefined) {
+            throw new NotFoundException(`Event not found`);
+        }
+
+        // TODO Uses corelated subqueries, may need optimization: fetch all TicketTypes and then just counts + merge results
+        const ticketTypesWithTicketCounts: TicketTypeWithTicketCounts[] = await eventRepository.query(
+            `SELECT tt.*,
+       (SELECT COUNT( *) FROM ticket WHERE ticket.ticketTypeId = tt.id AND ticket.status = ?) AS availableTicketCount,
+       (SELECT COUNT( *) FROM ticket WHERE ticket.ticketTypeId = tt.id AND ticket.status = ?) AS reservedTicketCount,
+       (SELECT COUNT( *) FROM ticket WHERE ticket.ticketTypeId = tt.id AND ticket.status = ?) AS soldTicketCount
+
+FROM ticket_type tt
+WHERE tt.eventId = ?;`,
+            [TICKET_STATUS_AVAILABLE, TICKET_STATUS_RESERVED, TICKET_STATUS_SOLD, event.id],
+        );
+
+        return {
+            // TODO format date time: event.startDateTime
+            event: event,
+            ticketTypes: ticketTypesWithTicketCounts,
+        };
     }
 }
