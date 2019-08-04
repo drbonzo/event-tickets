17c9d5afd8e1059b8e125942bd196aed708af50b
Michał Bąkowski <michal.bakowski@northslope.pl>
2019-08-04 19:39:21 +0200

    Added NestJS Validation

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import "reflect-metadata"; // needed by TypeORM
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
 
 const PORT = process.env.PORT || 3000;
 const HOSTNAME = process.env.HOSTNAME || "localhost";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.useGlobalPipes(new ValidationPipe());
     await app.listen(PORT, HOSTNAME);
 
     // tslint:disable-next-line:no-console
