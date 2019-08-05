8ab6db2b43c0295d15eab429ab6532ae6054c59c
Michał Bąkowski <michal.bakowski@northslope.pl>
2019-08-05 19:40:16 +0200

    Updated `GET /api/v1/events/:id` to return also available Tickets for each TicketType

--- src/api/v1/events/events.controller.ts
+++ src/api/v1/events/events.controller.ts
@@ -3,10 +3,12 @@ import { DATABASE_CONNECTION } from "../../../providers/provider-names";
 import { Connection } from "typeorm";
 import { EventEntity } from "../../../entity/EventEntity";
 import {
+    Ticket,
     TICKET_STATUS_AVAILABLE,
     TICKET_STATUS_RESERVED,
     TICKET_STATUS_SOLD,
 } from "../../../entity/Ticket";
+import { TicketType } from "../../../entity/TicketType";
 
 interface EventWithTicketCounts {
     id: number;
@@ -15,19 +17,26 @@ interface EventWithTicketCounts {
     availableTicketsCount: number;
 }
 
-interface TicketTypeWithTicketCounts {
+interface TicketCountsForTicketType {
     id: number;
-    name: string;
-    price: number;
-    sellingOption: string;
     availableTicketCount: number;
     reservedTicketCount: number;
     soldTicketCount: number;
 }
 
-interface EventWithDetails {
-    event: EventEntity;
-    ticketTypes: TicketTypeWithTicketCounts[];
+interface TicketTypeDetails {
+    ticketType: TicketType;
+    ticketCounts: TicketCountsForTicketType;
+    availableTicketIds: number[];
+}
+
+interface EventWithTicketTypesAndTickets {
+    event: {
+        id: number;
+        name: string;
+        startDateTime: number;
+    };
+    ticketTypes: TicketTypeDetails[];
 }
 
 @Controller("/api/v1/events")
@@ -43,18 +52,18 @@ export class EventsController {
         // TODO Uses corelated subquery - fetch all Events and then do separate count of Tickets + merge results
         const eventWithTicketCounts: EventWithTicketCounts[] = await eventRepository.query(
             `SELECT event_entity.id,
-                    event_entity.name,
-                    event_entity.startDateTime,
-                    (
-                      SELECT COUNT(*)
-                      FROM ticket_type tt
-                             LEFT JOIN ticket t on tt.id = t.ticketTypeId
-                      WHERE tt.eventId = event_entity.id
-                        and t.status = ?
-                    ) as availableTicketsCount
-             FROM event_entity
-             WHERE event_entity.startDateTime > ?
-             GROUP BY event_entity.id`,
+                        event_entity.name,
+                        event_entity.startDateTime,
+                        (
+                          SELECT COUNT(*)
+                          FROM ticket_type tt
+                                 LEFT JOIN ticket t on tt.id = t.ticketTypeId
+                          WHERE tt.eventId = event_entity.id
+                            and t.status = ?
+                        ) as availableTicketsCount
+                 FROM event_entity
+                 WHERE event_entity.startDateTime > ?
+                 GROUP BY event_entity.id`,
             [TICKET_STATUS_AVAILABLE, now],
         );
 
@@ -63,31 +72,81 @@ export class EventsController {
     }
 
     @Get("/:id")
-    public async getEventDetails(@Param("id") id: number): Promise<EventWithDetails> {
+    public async getEventDetails(@Param("id") id: number): Promise<EventWithTicketTypesAndTickets> {
         const eventRepository = this.databaseConnection.getRepository(EventEntity);
 
-        const event: EventEntity | undefined = await eventRepository.findOne(id);
+        // Find Event
+        const event: EventEntity | undefined = await eventRepository.findOne(id, {
+            relations: ["ticketTypes"],
+        });
 
         if (event === undefined) {
             throw new NotFoundException(`Event not found`);
         }
 
-        // TODO Uses corelated subqueries, may need optimization: fetch all TicketTypes and then just counts + merge results
-        const ticketTypesWithTicketCounts: TicketTypeWithTicketCounts[] = await eventRepository.query(
-            `SELECT tt.*,
-       (SELECT COUNT( *) FROM ticket WHERE ticket.ticketTypeId = tt.id AND ticket.status = ?) AS availableTicketCount,
-       (SELECT COUNT( *) FROM ticket WHERE ticket.ticketTypeId = tt.id AND ticket.status = ?) AS reservedTicketCount,
-       (SELECT COUNT( *) FROM ticket WHERE ticket.ticketTypeId = tt.id AND ticket.status = ?) AS soldTicketCount
+        // Collect TicketTypes
+        const ticketTypeDetailsMap: Map<number, TicketTypeDetails> = new Map();
 
-FROM ticket_type tt
-WHERE tt.eventId = ?;`,
+        event.ticketTypes.forEach(tt => {
+            const ticketTypeDetails: TicketTypeDetails = {
+                ticketType: tt,
+                ticketCounts: {
+                    id: tt.id,
+                    availableTicketCount: 0,
+                    reservedTicketCount: 0,
+                    soldTicketCount: 0,
+                },
+                availableTicketIds: [],
+            };
+            ticketTypeDetailsMap.set(tt.id, ticketTypeDetails);
+        });
+
+        // Count Tickets for each TicketType by Ticket.status
+        // TODO Uses corelated subqueries, may need optimization: fetch all TicketTypes and then just counts + merge results
+        const ticketTypesWithTicketCounts: TicketCountsForTicketType[] = await eventRepository.query(
+            `SELECT tt.id,
+                        (SELECT COUNT(*) FROM ticket WHERE ticket.ticketTypeId = tt.id AND ticket.status = ?) AS availableTicketCount,
+                        (SELECT COUNT(*) FROM ticket WHERE ticket.ticketTypeId = tt.id AND ticket.status = ?) AS reservedTicketCount,
+                        (SELECT COUNT(*) FROM ticket WHERE ticket.ticketTypeId = tt.id AND ticket.status = ?) AS soldTicketCount
+
+                 FROM ticket_type tt
+                 WHERE tt.eventId = ?;`,
             [TICKET_STATUS_AVAILABLE, TICKET_STATUS_RESERVED, TICKET_STATUS_SOLD, event.id],
         );
 
+        ticketTypesWithTicketCounts.forEach(ttwtc => {
+            const ticketTypeDetails = ticketTypeDetailsMap.get(ttwtc.id);
+            if (ticketTypeDetails) {
+                ticketTypeDetails.ticketCounts = { ...ttwtc };
+            }
+        });
+
+        // Find available Tickets for each TicketType
+        const ticketTypeIds = event.ticketTypes.map(tt => {
+            return tt.id;
+        });
+        const ticketsForTicketTypes: Ticket[] = await this.databaseConnection
+            .createQueryBuilder(Ticket, "ticket")
+            .where("ticketTypeId IN (:...ids)", { ids: ticketTypeIds })
+            .andWhere("status = :status", { status: TICKET_STATUS_AVAILABLE })
+            .innerJoinAndSelect("ticket.ticketType", "tt", "tt.id = ticket.ticketTypeId")
+            .getMany();
+
+        ticketsForTicketTypes.forEach(t => {
+            const ticketTypeDetails = ticketTypeDetailsMap.get(t.ticketType.id);
+            if (ticketTypeDetails) {
+                ticketTypeDetails.availableTicketIds.push(t.id);
+            }
+        });
+
         return {
-            // TODO format date time: event.startDateTime
-            event: event,
-            ticketTypes: ticketTypesWithTicketCounts,
+            event: {
+                id: event.id,
+                name: event.name,
+                // TODO format date time: event.startDateTime
+                startDateTime: event.startDateTime,
+            },
+            ticketTypes: Array.from(ticketTypeDetailsMap.values()),
         };
     }
 }
